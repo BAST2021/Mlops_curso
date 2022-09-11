6833b00577ec91e28871b6838a03683150564d2a
Bruno Alves da Silveira <91332508+BAST2021@users.noreply.github.com>
2022-09-11 13:47:19 -0300

    first commit

---
+++ src/app/main.py
@@ -0,0 +1,40 @@
+from flask import Flask,request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+from sklearn.linear_model import LinearRegression
+import pickle
+import os
+
+modelo =  pickle.load(open('../../models/modelo.sav','rb'))
+colunas = ['tamanho','ano','garagem']
+
+app=Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return "Minha primeira API"
+
+@app.route('/Sentimento/<frase>')
+@basic_auth.required
+
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang = 'pt_br', to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "Polaridade : {} ".format(polaridade)
+
+@app.route('/Cotacao/',methods =['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    cotacao = modelo.predict([dados_input])
+
+    return jsonify(cotacao = cotacao[0])
+
+app.run(debug =True, host='0.0.0.0')
+
